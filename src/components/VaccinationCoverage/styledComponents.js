abef6babee1ae37f3c6cb3bf73a79f767d6654fc
chaitu <chaitu@gxtechs.com>
2022-03-01 18:16:18 +0000

    t

---
+++ src/components/VaccinationCoverage/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/macro'
+
+export const VaccinationCoverageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #1c1c2b;
+  margin-bottom: 20px;
+  padding: 20px;
+`
+export const Heading = styled.h1`
+  color: #94a3b8;
+  font-family: 'Roboto';
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 1.5;
+`
